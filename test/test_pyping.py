b9145c3cdf7a31fe6ba6b73ab765828ec2cdc4a3
Thomas De Keulenaer <thomas.dekeulenaer@gmail.com>
2017-01-25 17:04:54 +0100

    Initial test for travis

---
+++ test/test_pyping.py
@@ -0,0 +1,34 @@
+import pytest
+
+import pyping
+
+
+def test_google_dns():
+    g_dns = '8.8.8.8'
+    r = pyping.ping(g_dns)
+    _asserts(r, g_dns)
+
+
+def _asserts(r, orig_dest):
+    assert r.destination == orig_dest
+
+    assert _test_float(r.avg_rtt)
+    assert _test_float(r.min_rtt)
+    assert _test_float(r.max_rtt)
+    avg_rtt = float(r.avg_rtt)
+    min_rtt = float(r.min_rtt)
+    max_rtt = float(r.max_rtt)
+
+    assert max_rtt > 0
+    assert min_rtt > 0
+    assert avg_rtt > 0
+    assert max_rtt >= avg_rtt
+    assert avg_rtt >= min_rtt
+
+
+def _test_float(f):
+    try:
+        float(f)
+        return True
+    except ValueError:
+        return False
